a0fbaba64df69c8faa8cc01b53a8237760e3447d
patricksimpson <izerop@gmail.com>
2016-03-29 22:15:00 -0400

    chore: clean up

--- app/models/wall.js
+++ app/models/wall.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   masterUrl: DS.attr('string'),
-  devices: DS.hasMany('device', { async: true }),
-  chenge: ''
+  devices: DS.hasMany('device', { async: true })
 });
--- app/routes/wall.js
+++ app/routes/wall.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Route.extend({
+  data: null,
   beforeModel: function() {
     this.get('session').open('firebase', { provider: 'custom', token: config.firebaseToken}).then(function(data) {
-      console.log(data);
+      this.set('data', data);
     });
   },
   model: function() {
