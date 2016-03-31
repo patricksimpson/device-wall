e4723f1cb2eb0c96340c093bc1f2a24ab2c4a764
patricksimpson <izerop@gmail.com>
2016-03-30 23:55:53 -0400

    feat: remove token generator

--- token/token.js
+++
@@ -1,5 +0,0 @@
-var FirebaseTokenGenerator = require("firebase-token-generator"),
-    tokenGenerator = new FirebaseTokenGenerator("<YOUR_FIREBASE_SECRET>"),
-    token = tokenGenerator.createToken({uid: "1", isAdmin: true});
-
-console.log(token);
