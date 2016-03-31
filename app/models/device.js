d09b2e706c4d68b75265cd21b38f1a3751d2e942
patricksimpson <izerop@gmail.com>
2016-03-30 23:33:11 -0400

    feat: adding slug to device model

--- app/models/device.js
+++ app/models/device.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  url: DS.attr('string')
+  url: DS.attr('string'),
+  slug: DS.attr('string'),
+  updated: DS.attr('number')
 });
