eec6dd52ac95e58727c5f1b54ae86a9f49324a3a
patricksimpson <izerop@gmail.com>
2016-04-03 08:01:59 -0400

    feat: moving token gen to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
+import config from '../config/environment';
+import FirebaseTokenGenerator from 'npm:firebase-token-generator';
 
 export default Ember.Route.extend({
   beforeModel() {
+    if (this.get('session').get('uid') !== '1') {
+      var tokenGenerator = new FirebaseTokenGenerator(config.firebaseSecret),
+          token = tokenGenerator.createToken({ uid: '1', isAdmin: true });
+      return this.get('session').open('firebase', { provider: 'custom', token: token});
+    } else {
+      return this.get('session');
+    }
+  },
+  afterModel() {
     this.transitionTo('wall');
   }
 });
--- app/routes/wall.js
+++ app/routes/wall.js
@@ -1,18 +1,6 @@
 import Ember from 'ember';
-import config from '../config/environment';
-import FirebaseTokenGenerator from 'npm:firebase-token-generator';
 
 export default Ember.Route.extend({
-  data: null,
-  beforeModel: function() {
-    if (this.get('session').get('uid') !== '1') {
-      var tokenGenerator = new FirebaseTokenGenerator(config.firebaseSecret),
-          token = tokenGenerator.createToken({ uid: '1', isAdmin: true });
-      this.get('session').open('firebase', { provider: 'custom', token: token}).then(function(data) {
-        console.log(data);
-      });
-    }
-  },
   model: function() {
     return this.store.findAll('wall');
   }
