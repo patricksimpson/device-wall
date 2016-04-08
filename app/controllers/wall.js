18a025ddeec13e53668dade3f7fa6f665782faa3
patricksimpson <izerop@gmail.com>
2016-04-08 16:49:37 -0400

    feat: adding redirect

--- app/controllers/wall.js
+++ app/controllers/wall.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isRedirecting: true,
+  actions: {
+    toggleRedirect() {
+      let isRedirecting = !this.get('isRedirecting');
+      this.set('isRedirecting', isRedirecting);
+    }
+  }
 });
