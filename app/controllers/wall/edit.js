705f9ffa6fd64ef01d05e1d6dc835482bcedc5ba
patricksimpson <izerop@gmail.com>
2016-04-10 15:35:50 -0400

    feat: editing master url now updates all children devices

--- app/controllers/wall/edit.js
+++ app/controllers/wall/edit.js
@@ -4,8 +4,17 @@ export default Ember.Controller.extend({
   wall: Ember.computed(function() { return this.get('model'); }),
   actions: {
     editWall() {
-      console.log(this.get('wall.name'));
-      console.log(this.get('wall.masterUrl'));
+      let url = this.get('wall.masterUrl'),
+          _this = this;
+      this.get('wall.devices').then(function(d) {
+        d.currentState.forEach(function(device) {
+          d.store.findRecord('device', device.id).then(function(data) {
+            data.set('url', url);
+            data.save();
+          });
+        });
+        _this.transitionToRoute('wall');
+      });
     }
   }
 });
